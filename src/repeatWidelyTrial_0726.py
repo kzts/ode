3d4a1e0bd8719bb43d587fb26bf567cdf6a90106
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2016-08-22 21:06:58 +0900

    erase commentout

--- src/repeatWidelyTrial_0726.py
+++ src/repeatWidelyTrial_0726.py
@@ -15,13 +15,6 @@ print cmd_Mkdir
 
 os.system(cmd_Mkdir)
 
-#Prs_Sld_max = 0.2
-#Prs_Elb_max = 0.2
-#Prs_Wst_max = 0.1
-#Prs_Sld_min = 0.1
-#Prs_Elb_min = 0.1
-#Prs_Wst_min = 0.05
-
 #Prs_Sld_max = + 5.0
 #Prs_Elb_max = + 5.0
 #Prs_Wst_max = + 5.0
@@ -35,10 +28,6 @@ Prs_Wst_max = + 10.0
 Prs_Sld_min = - 10.0
 Prs_Elb_min = - 10.0
 Prs_Wst_min = - 10.0
-
-#Prs_Bnd = 0.0
-#Time_loop = "200"
-#waitTime  = 2
 
 N = 1000
 #N = 500
@@ -55,8 +44,5 @@ for i in range(0,N):
     cmd = "./hopper2 " + str(Prs_Sld) + "\t" + str(Prs_Elb) + "\t" + str(Prs_Wst) + "\t" + DirName2    
     #print cmd
     os.system(cmd)
-    #time.sleep(waitTime)
-    #ratio = 100* i/ N
-    #print ratio
 
 
